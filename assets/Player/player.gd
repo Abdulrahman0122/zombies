06a8ae9cfb4d5b15d75ee33d3b1f76ce5505ab9f
Abdulrahman0122 <abdulrahmangg101@gmail.com>
2026-03-15 17:52:42 +0200

    i added a anime

--- assets/Player/player.gd
+++ assets/Player/player.gd
@@ -1,23 +1,24 @@
 extends CharacterBody3D
 
-
 @export var SPEED = 5.0
 @export var JUMP_VELOCITY = 4.5
+@export var sensitivity=0.004
 @export var gravity =-9.8
 @onready var head: Node3D =$head
 @onready var camera_3d: Camera3D =$head/Camera3D
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
-
-pass
+	pass
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion :
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation_x= clamp()
+		head.rotate_y(-event.relative.x * sensitivity)
+		camera_3d.rotate_x(-event.relative.y * sensitivity)
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+	
+	pass
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -30,7 +31,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
---
+++ assets/Zombie/zombie.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+@export var speed = 5
+@export var gravity = -10
+
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity
+	
+	
+	
+	move_and_slide()
+	
+	pass
